5f096e0ababcb0ca570f7b2ca83c9a1df3d3ad1f
gfmartinez <55528370+gmartinez1985@users.noreply.github.com>
2025-06-30 15:32:08 +0200

    Update Solution.java

--- categories/arrays-and-hashing/contains-duplicate/Solution.java
+++ categories/arrays-and-hashing/contains-duplicate/Solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        HashSet<Integer> numsSet = new HashSet<Integer>();
-
+        Set<Integer> numsSet = new HashSet<Integer>();
         for(int i= 0; i<nums.length; i++) {
             if(numsSet.contains(nums[i])) {
                 return true;
             }
             numsSet.add(nums[i]);
         }
-
         return false;       
     }
 }
